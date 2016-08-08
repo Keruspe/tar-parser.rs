fe60b02ad1083b8db37f855a95aa3e65bc60c385
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-08-08 17:56:30 +0200

    add a long name parsing test to simple example
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -4,9 +4,8 @@ extern crate nom;
 use tar::*;
 use nom::IResult;
 
-fn main() {
-    let tar = include_bytes!("../test.tar");
-    match parse_tar(tar) {
+fn test_parse_tar(i: &[u8]) {
+    match parse_tar(i) {
         IResult::Done(_, entries) => {
             for e in entries.iter() {
                 println!("{:?}", e);
@@ -17,4 +16,13 @@ fn main() {
             panic!("cannot parse tar archive");
         }
     }
+}
+
+fn main() {
+    let test = include_bytes!("../test.tar");
+    let long = include_bytes!("../long.tar");
+    println!("parse test");
+    test_parse_tar(test);
+    println!("parse long");
+    test_parse_tar(long);
 }
