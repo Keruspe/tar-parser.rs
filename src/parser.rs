68ab544677abcf76233ab79feb962543f8b55dbd
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-09-03 16:03:04 +0200

    port to current nom git and do_parse
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/parser.rs
+++ src/parser.rs
@@ -103,24 +103,23 @@ pub struct Padding;
 
 macro_rules! take_str_eat_garbage (
     ( $i:expr, $size:expr ) => (
-        chain!($i,
-            s: map_res!(take_until!("\0"), from_utf8)                  ~
-            length: expr_opt!({($size as usize).checked_sub(s.len())}) ~
-            take!(length),
-            ||{
-                s
-            }
+        do_parse!($i,
+            s:      map_res!(take_until!("\0"), from_utf8)             >>
+            length: expr_opt!({($size as usize).checked_sub(s.len())}) >>
+            take!(length)                                              >>
+            (s)
         )
     );
 );
 
-named!(parse_str4<&[u8], &str>, take_str_eat_garbage!(4));
-named!(parse_str8<&[u8], &str>, take_str_eat_garbage!(8));
-named!(parse_str32<&[u8], &str>, take_str_eat_garbage!(32));
+named!(parse_str4<&[u8], &str>,   take_str_eat_garbage!(4));
+named!(parse_str8<&[u8], &str>,   take_str_eat_garbage!(8));
+named!(parse_str32<&[u8], &str>,  take_str_eat_garbage!(32));
 named!(parse_str100<&[u8], &str>, take_str_eat_garbage!(100));
 named!(parse_str155<&[u8], &str>, take_str_eat_garbage!(155));
 named!(parse_str512<&[u8], &str>, take_str_eat_garbage!(512));
 
+/* TODO: drop me / simplify me */
 macro_rules! take_until_expr_with_limit_consume(
   ($i:expr, $submac:ident!( $($args:tt)* ), $stop: expr, $limit: expr) => (
     {
@@ -192,7 +191,7 @@ fn parse_octal(i: &[u8], n: usize) -> IResult<&[u8], u64> {
     map_res!(i, take_str_eat_garbage!(n), octal_to_u64)
 }
 
-named!(parse_octal8<&[u8], u64>, apply!(parse_octal, 8));
+named!(parse_octal8<&[u8], u64>,  apply!(parse_octal, 8));
 named!(parse_octal12<&[u8], u64>, apply!(parse_octal, 12));
 
 /*
@@ -201,19 +200,19 @@ named!(parse_octal12<&[u8], u64>, apply!(parse_octal, 12));
 
 fn char_to_type_flag(c: char) -> TypeFlag {
     match c {
-        '0' | '\0' => TypeFlag::NormalFile,
-        '1' => TypeFlag::HardLink,
-        '2' => TypeFlag::SymbolicLink,
-        '3' => TypeFlag::CharacterSpecial,
-        '4' => TypeFlag::BlockSpecial,
-        '5' => TypeFlag::Directory,
-        '6' => TypeFlag::FIFO,
-        '7' => TypeFlag::ContiguousFile,
-        'g' => TypeFlag::PaxInterexchangeFormat,
-        'x' => TypeFlag::PaxExtendedAttributes,
-        'L' => TypeFlag::GNULongName,
+        '0' | '\0'  => TypeFlag::NormalFile,
+        '1'         => TypeFlag::HardLink,
+        '2'         => TypeFlag::SymbolicLink,
+        '3'         => TypeFlag::CharacterSpecial,
+        '4'         => TypeFlag::BlockSpecial,
+        '5'         => TypeFlag::Directory,
+        '6'         => TypeFlag::FIFO,
+        '7'         => TypeFlag::ContiguousFile,
+        'g'         => TypeFlag::PaxInterexchangeFormat,
+        'x'         => TypeFlag::PaxExtendedAttributes,
+        'L'         => TypeFlag::GNULongName,
         'A' ... 'Z' => TypeFlag::VendorSpecific,
-        _ => TypeFlag::NormalFile
+        _           => TypeFlag::NormalFile
     }
 }
 
@@ -227,7 +226,7 @@ named!(parse_type_flag<&[u8], TypeFlag>, map!(take!(1), bytes_to_type_flag));
  * Sparse parsing
  */
 
-named!(parse_one_sparse<&[u8], Sparse>, map!(pair!(parse_octal12, parse_octal12), |(offset, numbytes)| Sparse { offset: offset, numbytes: numbytes }));
+named!(parse_one_sparse<&[u8], Sparse>, do_parse!(offset: parse_octal12 >> numbytes: parse_octal12 >> (Sparse { offset: offset, numbytes: numbytes })));
 
 fn parse_sparses_with_limit(i: &[u8], limit: usize) -> IResult<&[u8], Vec<Sparse>> {
     take_until_expr_with_limit_consume!(i, parse_one_sparse, |s: &Sparse| s.offset == 0 && s.numbytes == 0, limit)
@@ -240,14 +239,12 @@ fn add_to_vec<'a, 'b>(sparses: &'a mut Vec<Sparse>, extra: Vec<Sparse>) -> &'a m
 
 fn parse_extra_sparses<'a, 'b>(i: &'a [u8], isextended: bool, sparses: &'b mut Vec<Sparse>) -> IResult<&'a [u8], &'b mut Vec<Sparse>> {
     if isextended {
-        chain!(i,
-            sps:           apply!(parse_sparses_with_limit, 21) ~
-            extended:      parse_bool                           ~
-            take!(7) /* padding to 512 */                       ~
-            extra_sparses: apply!(parse_extra_sparses, extended, add_to_vec(sparses, sps)),
-            ||{
-                extra_sparses
-            }
+        do_parse!(i,
+            sps:           apply!(parse_sparses_with_limit, 21)                            >>
+            extended:      parse_bool                                                      >>
+            take!(7) /* padding to 512 */                                                  >>
+            extra_sparses: apply!(parse_extra_sparses, extended, add_to_vec(sparses, sps)) >>
+            (extra_sparses)
         )
     } else {
         IResult::Done(i, sparses)
@@ -271,28 +268,26 @@ named!(parse_bool<&[u8], bool>, map!(take!(1), to_bool));
 fn parse_ustar00_extra_pax(i: &[u8]) -> IResult<&[u8], PaxHeader> {
     let mut sparses = Vec::new();
 
-    chain!(i,
-        atime: parse_octal12 ~
-        ctime: parse_octal12 ~
-        offset: parse_octal12 ~
-        longnames: parse_str4 ~
-        take!(1) ~
-        sps: apply!(parse_sparses_with_limit, 4) ~
-        isextended: parse_bool ~
-        realsize: parse_octal12 ~
-        take!(17) ~ /* padding to 512 */
-        apply!(parse_extra_sparses, isextended, add_to_vec(&mut sparses, sps)),
-        ||{
-            PaxHeader {
-                atime:         atime,
-                ctime:         ctime,
-                offset:        offset,
-                longnames:     longnames,
-                sparses:       sparses,
-                isextended:    isextended,
-                realsize:      realsize,
-            }
-        }
+    do_parse!(i,
+        atime: parse_octal12                                                   >>
+        ctime: parse_octal12                                                   >>
+        offset: parse_octal12                                                  >>
+        longnames: parse_str4                                                  >>
+        take!(1)                                                               >>
+        sps: apply!(parse_sparses_with_limit, 4)                               >>
+        isextended: parse_bool                                                 >>
+        realsize: parse_octal12                                                >>
+        take!(17) /* padding to 512 */                                         >>
+        apply!(parse_extra_sparses, isextended, add_to_vec(&mut sparses, sps)) >>
+        (PaxHeader {
+            atime:         atime,
+            ctime:         ctime,
+            offset:        offset,
+            longnames:     longnames,
+            sparses:       sparses,
+            isextended:    isextended,
+            realsize:      realsize,
+        })
     )
 }
 
@@ -300,33 +295,37 @@ fn parse_ustar00_extra_pax(i: &[u8]) -> IResult<&[u8], PaxHeader> {
  * UStar Posix parsing
  */
 
-named!(parse_ustar00_extra_posix<&[u8], UStarExtraHeader>, map!(pair!(parse_str155, take!(12)), |(prefix, _)| UStarExtraHeader::PosixUStar(PosixUStarHeader { prefix: prefix })));
+named!(parse_ustar00_extra_posix<&[u8], UStarExtraHeader>, do_parse!(prefix: parse_str155 >> take!(12) >> (UStarExtraHeader::PosixUStar(PosixUStarHeader { prefix: prefix }))));
 
 fn parse_ustar00_extra<'a, 'b>(i: &'a [u8], flag: &'b TypeFlag) -> IResult<&'a [u8], UStarExtraHeader<'a>> {
     match *flag {
-        TypeFlag::PaxInterexchangeFormat => map!(i, parse_ustar00_extra_pax, |header|{ UStarExtraHeader::Pax(header) }),
+        TypeFlag::PaxInterexchangeFormat => do_parse!(i, header: parse_ustar00_extra_pax >> (UStarExtraHeader::Pax(header))),
         _                                => parse_ustar00_extra_posix(i)
     }
 }
 
 fn parse_ustar00<'a, 'b>(i: &'a [u8], flag: &'b TypeFlag) -> IResult<&'a [u8], ExtraHeader<'a>> {
-    map!(i, tuple!(tag!("00"), parse_str32, parse_str32, parse_octal8, parse_octal8, apply!(parse_ustar00_extra, flag)),
-        |(_, uname, gname, devmajor, devminor, extra)|{
-            ExtraHeader::UStar(UStarHeader {
-                magic:    "ustar\0",
-                version:  "00",
-                uname:    uname,
-                gname:    gname,
-                devmajor: devmajor,
-                devminor: devminor,
-                extra:    extra
-            })
-        }
+    do_parse!(i,
+        tag!("00")                               >>
+        uname: parse_str32                       >>
+        gname: parse_str32                       >>
+        devmajor: parse_octal8                   >>
+        devminor: parse_octal8                   >>
+        extra: apply!(parse_ustar00_extra, flag) >>
+        (ExtraHeader::UStar(UStarHeader {
+            magic:    "ustar\0",
+            version:  "00",
+            uname:    uname,
+            gname:    gname,
+            devmajor: devmajor,
+            devminor: devminor,
+            extra:    extra
+        }))
     )
 }
 
 fn parse_ustar<'a, 'b>(i: &'a [u8], flag: &'b TypeFlag) -> IResult<&'a [u8], ExtraHeader<'a>> {
-    map!(i, pair!(tag!("ustar\0"), apply!(parse_ustar00, flag)), |(_, ustar)| ustar)
+    do_parse!(i, tag!("ustar\0") >> ustar: apply!(parse_ustar00, flag) >> (ustar))
 }
 
 /*
@@ -335,6 +334,7 @@ fn parse_ustar<'a, 'b>(i: &'a [u8], flag: &'b TypeFlag) -> IResult<&'a [u8], Ext
 
 named!(parse_posix<&[u8], ExtraHeader>, map!(take!(255), |_| ExtraHeader::Padding)); /* padding to 512 */
 
+/* TODO: port to do_parse! */
 fn parse_header<'a>(i: &'a [u8]) -> IResult<&'a [u8], PosixHeader<'a>> {
     let mut err_res = None;
     let res = chain!(i,
@@ -405,22 +405,20 @@ fn parse_contents(i: &[u8], size: u64) -> IResult<&[u8], &[u8]> {
         0 => 0,
         t => 512 - t
     };
-    map!(i, pair!(take!(size as usize), take!(padding as usize)), |(contents, _)| contents)
+    do_parse!(i, contents: take!(size as usize) >> take!(padding as usize) >> (contents))
 }
 
 /*
  * Tar entry header + contents parsing
  */
 
-named!(parse_entry<&[u8], TarEntry>, chain!(
-    header:   parse_header ~
-    contents: apply!(parse_contents, header.size),
-    ||{
-        TarEntry {
+named!(parse_entry<&[u8], TarEntry>, do_parse!(
+    header:   parse_header                        >>
+    contents: apply!(parse_contents, header.size) >>
+    (TarEntry {
             header: header,
             contents: contents
-        }
-    }
+    })
 ));
 
 /*
@@ -433,7 +431,7 @@ fn filter_entries(entries: Vec<TarEntry>) -> Vec<TarEntry> {
 }
 
 pub fn parse_tar(i: &[u8]) -> IResult<&[u8], Vec<TarEntry>> {
-    map!(i, pair!(map!(many0!(parse_entry), filter_entries), eof), |(entries, _)| entries)
+    do_parse!(i, entries: map!(many0!(parse_entry), filter_entries) >> eof!() >> (entries))
 }
 
 /*
